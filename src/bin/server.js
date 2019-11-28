cd442819e6a618584ba37cf4d39163c6a54edeb5
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2019-11-27 22:35:24 -0300

    add cors in api

--- src/bin/server.js
+++ src/bin/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('../routes');
 const app = express();
 
 require('../database');
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
